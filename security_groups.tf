57a661fcca612f5b1d74bb3177e39b0c5feeaf4e
Jonathan Dietz <jonathandietz@users.noreply.github.com>
2016-12-06 12:43:32 -0800

    changing ICMP codes to a wildcard

--- security_groups.tf
+++ security_groups.tf
@@ -48,8 +48,8 @@ resource "aws_security_group_rule" "rancher_manager_ha1" {
 
 resource "aws_security_group_rule" "rancher_manager_icmp" {
     type = "ingress"
-    from_port = 0
-    to_port = 0
+    from_port = "-1"
+    to_port = "-1"
     protocol = "icmp"
     source_security_group_id = "${aws_security_group.rancher_manager.id}"
     security_group_id = "${aws_security_group.rancher_manager.id}"
